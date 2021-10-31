83d9c77368e30db52c2fbae673525fa4aa7e6ab6
Satvik Kasinadhuni <rachitipidudraksha@gmail.com>
2021-10-31 18:44:26 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 
 import Header from './components/Header';
 import About from './components/About';
-
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <About />
+      <Footer />
     </div>
   );
 }
