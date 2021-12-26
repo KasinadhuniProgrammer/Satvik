4eb55acd419aeb7ac0f37c5155eb3193cded08f8
Satvik Kasinadhuni <rachitipidudraksha@gmail.com>
2021-12-26 08:10:05 -0500

    added social media links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Typing from 'react-typing-animation';
 import './css/Header.css'; 
 
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
 
 const AnimatedComponent = () => (
     <Typing loop={true}>
@@ -24,6 +28,17 @@ function Header() {
                <center><h1 id="header__h1">Satvik Kasinadhuni</h1></center>
                <hr id="header__seperator"/>
                <center><h3 id="header__h3"><AnimatedComponent /></h3></center>
+               <div className="header__links">
+                   <div className="links__linkedin">
+                       <a className="linkedin__anchor" href="https://www.linkedin.com/in/satvik-kasinadhuni/" target="_blank"><LinkedInIcon /></a>
+                   </div>
+                   <div className="links__twitter">
+                       <a className="twitter__anchor" href="https://twitter.com/SatvikJS" target="_blank"><TwitterIcon /></a>
+                   </div>
+                   <div className="links__github">
+                       <a className="github__anchor" href="https://github.com/KasinadhuniProgrammer" target="_blank"><GitHubIcon /></a>
+                   </div>
+               </div>
                <div className="header__btn">
                    <a id="button__btn" href="#about">Continue ▼</a>
                </div>
