e71c2f7b5d3ebfbd1d1b283a35cbf1c9032dfd82
Satvik Kasinadhuni <rachitipidudraksha@gmail.com>
2022-06-22 07:35:39 +0530

    new design

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React from "react";
-import './App.css';
-
-import Header from './components/Header';
-import About from './components/About';
-import Footer from './components/Footer';
+import React from "react"
+import './App.css'
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <About />
-      <Footer />
+      
+      <div className="header">
+
+        <h1>Satvik Kasinadhuni</h1>
+
+      </div>
+
+      <div className="main">
+
+        <p id="one">Hi. I'm Satvik Kasinadhuni, a Software geek and Cricket enthusiast from Connecticut.</p>
+
+        <p id="two">I touched into coding at the age of 10 but started taking it more seriously since 2 years back. I love to learn and work with React, JavaScript, TypeScript, PostgreSQL, MongoDB, Firebase, and many more things. I have experience in Software Engineering from interning at <a id="link" href="https://ipserlab.com/" target="_blank">IpserLab</a>, a startup company based in Mountain View, CA. I interned there from Feb 2021 - Dec 2021 as a Frontend Developer, and then started interning there again in June as a WebRTC Engineer.</p>
+
+        <p id="three">When I am not coding, the only thing I am doing is watching Cricket. Cricket has been part of me ever since I was a small child. I love watching international matches and domestic franchises such as the IPL. My favorite format of the sport is Test Cricket. Even though it is very long, I think it truly shows what cricket is about, and I love spending hours and hours watching matches and looking at analysis/highlights.</p>
+
+      </div>
+
+
+      <div className="closing">
+
+        <p>You can find me on <a id="github" href="https://github.com/KasinadhuniProgrammer" target="_blank">GitHub</a>, <a id="linkedin" href="https://www.linkedin.com/in/satvik-k/" target="_blank">Linkedin</a>, or by shooting me an <a id="mail" href="mailto:skasinad@gmail.com">email</a>!</p>
+
+      </div>
+
+      <div className="footer">
+
+        <span>made by satvik | copyright <a id="github" href="https://github.com/KasinadhuniProgrammer">@KasinadhuniProgrammer</a></span>
+
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
